9bcdcb8d143ccdff4bd70d1ae342bf52f14b0fd1
Seungjae Lee <seungjaelee@Seungjaes-MacBook-Pro.local>
2019-09-03 20:10:57 +0900

    first commit

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public VirtualJoystick moveJoystick;
+    public float moveSpeed = 0.01f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+
+    private Rigidbody controller;
+    private Transform camTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Rigidbody>();
+        controller.maxAngularVelocity = terminalRotationSpeed;
+        controller.drag = drag;
+
+        camTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        if (dir.magnitude > 1)
+            dir.Normalize();
+
+        if (moveJoystick.InputDirection != Vector3.zero)
+        {
+            dir = moveJoystick.InputDirection;
+            Vector3 rotatedDir = camTransform.TransformDirection(dir);
+            rotatedDir = new Vector3(rotatedDir.x, 0, rotatedDir.z);
+            rotatedDir = rotatedDir.normalized * dir.magnitude;
+
+
+            float angle = Mathf.Atan2(dir.z, dir.x);
+
+            // Quaternion rotation = Quaternion.LookRotation(controller.transform.position, new Vector3(0, angle, 0));
+            // controller.transform.rotation = new Quaternion(0, angle, 0, 0);
+
+            Quaternion rotation = Quaternion.LookRotation(dir, Vector3.zero);
+            rotation *= Quaternion.Euler(0, -90, 0);
+            controller.transform.rotation = rotation;
+            controller.transform.position += rotatedDir * moveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;   // Image
+using UnityEngine.EventSystems; // PointerEventData
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Image joystickImg;
+
+    public Vector3 InputDirection { set; get; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+        InputDirection = Vector3.zero;
+    }
+
+    // Update is called once per frame
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Debug.Log("OnDrag");
+        Vector2 pos = Vector2.zero;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                bgImg.rectTransform,
+                ped.position, // current pointer position
+                ped.pressEventCamera,
+                out pos
+             )
+           )
+        {
+            Debug.Log("pos.x:" + pos.x + ", pos.y:" + pos.y + ", pivot.x:" + bgImg.rectTransform.pivot.x + ", pivot.y:" + bgImg.rectTransform.pivot.y+", sd.x:"+bgImg.rectTransform.sizeDelta.x + ", sd.y:"+bgImg.rectTransform.sizeDelta.y);
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x); // relative position inside bgImg
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y); // (0,300) -> (0,1)
+
+            float x = (bgImg.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1; // change scale (0,1) -> (-1,-1)
+            float y = (bgImg.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1; // *2 -> -1
+
+            InputDirection = new Vector3(x, 0, y);
+            InputDirection = (InputDirection.magnitude > 1) ? InputDirection.normalized : InputDirection;
+
+            joystickImg.rectTransform.anchoredPosition = new Vector3(
+                InputDirection.x * (bgImg.rectTransform.sizeDelta.x / 3),
+                InputDirection.z * (bgImg.rectTransform.sizeDelta.y / 3));
+
+            Debug.Log("pos.x: "+ pos.x + ", pos.y:"+ pos.y + ", x:" + x + ", y:" + y);
+        }
+
+    }
+
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        Debug.Log("OnPointerDown");
+        OnDrag(ped);
+    }
+
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        Debug.Log("OnPointerUp");
+        InputDirection = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
